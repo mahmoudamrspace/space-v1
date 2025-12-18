bd94ab5a58eb18c610b5972aa790115393bc7181
mahmoudamrspace <mahmoudamr.space@gmail.com>
2025-12-18 23:00:26 +0200

    Enhance sky with realistic stars and smooth shooting stars
    - Add more stars with realistic color variations (white, blue-white, yellow-white, orange)
    - Add twinkling stars layer with natural brightness variation
    - Add slow parallax drift animation for depth
    - Add rare, smooth shooting stars with glowing trails
    - Enhance background with subtle nebula gradients
    - Improve star distribution for more realistic appearance
    - Optimize animations for smooth performance

--- src/components/sky.tsx
+++ src/components/sky.tsx
@@ -3,8 +3,11 @@ import "../components/sky.css";
 
 const Sky = () => (
   <div id='sky'>
-    {/* Far layer - background stars */}
-    <span>
+    {/* Far layer - background stars (many small stars) */}
+    <span className='star-layer far'>
+      <s></s>
+      <s></s>
+      <s></s>
       <s></s>
       <s></s>
       <s></s>
@@ -12,21 +15,36 @@ const Sky = () => (
       <s></s>
     </span>
     {/* Mid layer - medium stars */}
-    <span>
+    <span className='star-layer mid'>
       <s></s>
-      <s></s>
-      <s></s>
-      <s></s>
-      <s></s>
-    </span>
-    {/* Near layer - foreground stars */}
-    <span>
       <s></s>
       <s></s>
       <s></s>
       <s></s>
       <s></s>
     </span>
+    {/* Near layer - foreground stars (bright, larger) */}
+    <span className='star-layer near'>
+      <s></s>
+      <s></s>
+      <s></s>
+      <s></s>
+      <s></s>
+    </span>
+    {/* Twinkling stars layer */}
+    <span className='star-layer twinkle'>
+      <s className='twinkle-star'></s>
+      <s className='twinkle-star'></s>
+      <s className='twinkle-star'></s>
+      <s className='twinkle-star'></s>
+      <s className='twinkle-star'></s>
+    </span>
+    {/* Shooting stars - rare, random appearances with smooth motion */}
+    <div className='shooting-star' style={{ '--delay': '12s', '--duration': '3.5s', '--start-x': '10%', '--start-y': '20%', '--move-x': '80vw', '--move-y': '60vh' } as React.CSSProperties}></div>
+    <div className='shooting-star' style={{ '--delay': '45s', '--duration': '4s', '--start-x': '80%', '--start-y': '5%', '--move-x': '-60vw', '--move-y': '90vh' } as React.CSSProperties}></div>
+    <div className='shooting-star' style={{ '--delay': '78s', '--duration': '3.8s', '--start-x': '50%', '--start-y': '0%', '--move-x': '0vw', '--move-y': '100vh' } as React.CSSProperties}></div>
+    <div className='shooting-star' style={{ '--delay': '115s', '--duration': '4.2s', '--start-x': '15%', '--start-y': '30%', '--move-x': '70vw', '--move-y': '40vh' } as React.CSSProperties}></div>
+    <div className='shooting-star' style={{ '--delay': '152s', '--duration': '3.6s', '--start-x': '70%', '--start-y': '15%', '--move-x': '-40vw', '--move-y': '70vh' } as React.CSSProperties}></div>
   </div>
 );
 
